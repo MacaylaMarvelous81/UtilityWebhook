a2aaa620fa6186e0300f7b24378a8a64d6acc702
MacaylaMarvelous81 <admin@thelegendaryorb.tk>
2020-05-07 21:14:09 -0700

    If packet recieved was sent by player, run command

--- server.go
+++ server.go
@@ -15,20 +15,22 @@ func main() {
 		player.SendMessage("Welcome! You have been connected to the external service. Type .help in chat for commands!")
 		player.OnPlayerMessage(func(event *event.PlayerMessage) {
 			if event.MessageType == "chat" {
-				if event.Message == ".help" {
-					player.SendMessage("Command List:\n.help - Shows this help menu\n.love - Spawns heart particles on you\n.angry - Spawns angry particles on you")
-				}
-				if event.Message == ".love" {
-					player.Position(func(position mctype.Position) {
-						world.SpawnParticle("minecraft:heart_particle", position)
-						player.SendMessage("Heart particles created!")
-					})
-				}
-				if event.Message == ".angry" {
-					player.Position(func(position mctype.Position) {
-						world.SpawnParticle("minecraft:villager_angry", position)
-						player.SendMessage("Angry particles created!")
-					})
+				if event.Sender == player.Name() {
+					if event.Message == ".help" {
+						player.SendMessage("Command List:\n.help - Shows this help menu\n.love - Spawns heart particles on you\n.angry - Spawns angry particles on you")
+					}
+					if event.Message == ".love" {
+						player.Position(func(position mctype.Position) {
+							world.SpawnParticle("minecraft:heart_particle", position)
+							player.SendMessage("Heart particles created!")
+						})
+					}
+					if event.Message == ".angry" {
+						player.Position(func(position mctype.Position) {
+							world.SpawnParticle("minecraft:villager_angry", position)
+							player.SendMessage("Angry particles created!")
+						})
+					}
 				}
 			}
 		})
