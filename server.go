cb2fd4e6753e546704aafa74f74ab88b030fb217
MacaylaMarvelous81 <admin@thelegendaryorb.tk>
2020-05-07 21:01:59 -0700

    .angry

--- server.go
+++ server.go
@@ -16,12 +16,18 @@ func main() {
 		player.OnPlayerMessage(func(event *event.PlayerMessage) {
 			if event.MessageType == "chat" {
 				if event.Message == ".help" {
-					player.SendMessage("Command List:\n.help - Shows this help menu\n.love - Spawns heart particles on you")
+					player.SendMessage("Command List:\n.help - Shows this help menu\n.love - Spawns heart particles on you\n.angry - Spawns angry particles on you")
 				}
 				if event.Message == ".love" {
 					player.Position(func(position mctype.Position) {
 						world.SpawnParticle("minecraft:heart_particle", position)
 						player.SendMessage("Heart particles created!")
+					})
+				}
+				if event.Message == ".angry" {
+					player.Position(func(position mctype.Position) {
+						world.SpawnParticle("minecraft:villager_angry", position)
+						player.SendMessage("Angry particles created!")
 					})
 				}
 			}
